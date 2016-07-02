998d780bd36711111973487cfbd0646d2586a9c5
Gdsutton <gdsutton@outlook.com>
2016-07-02 23:20:44 +0100

    added plots

--- fitdistrplus.R
+++ fitdistrplus.R
@@ -13,4 +13,23 @@ plotdist(groundbeef[,1], histo = TRUE, demp = TRUE)
 
 descdist(gb, boot = 1000)
 
+fw <- fitdist(gb, "weibull")
+
+plot(fw)
+summary(fw)
+
+fg <- fitdist(gb, "gamma")
+fl <- fitdist(gb, "lnorm")
+fn <- fitdist(gb, "norm")
+
+distfits <- list(fw,fg,fl,fn)
+distlegend <- c("weib","gam","lnorm","norm")
+par(mfrow = c(2,2))
+denscomp(distfits, legendtext = distlegend)
+qqcomp(distfits, legendtext = distlegend)
+cdfcomp(distfits, legendtext = distlegend)
+ppcomp(distfits, legendtext = distlegend)
+
+
+
 
